91125db36939f930bdff3debed219c390276633d
vaibhav.kumar <vaibhav.kumar@jungleworks.com>
2022-11-26 12:58:43 +0530

    user login validator

--- modules/user/index.js
+++ modules/user/index.js
@@ -3,3 +3,4 @@ const validator = require('./validator');
 const controller = require('./controller');
 
 app.post('/auth/register', validator.registerUser, controller.registerUser);
+app.post('/auth/login',    validator.loginUser);
--- modules/user/validator.js
+++ modules/user/validator.js
@@ -2,6 +2,7 @@
 const Joi = require('joi');
 
 exports.registerUser = registerUser;
+exports.loginUser    = loginUser;
 
 function validate(opts, schema) {
     const validate = schema.validate(opts);
@@ -19,6 +20,25 @@ async function registerUser(req, res, next) {
     const validateFields = validate(req.body, schema);
     if(validateFields.error) {
         return res.status(400).send("INVALID REQUEST");
+    }
+
+    next();
+
+}
+
+
+async function loginUser(req, res, next) {
+
+    const schema = Joi.object().keys({
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+        user_type: Joi.number().required().valid(1,2)
+    })
+
+    const validateFields = validate(req.body, schema);
+
+    if(validateFields.error) {
+        res.status(400).send("INVALID REQUEST");
     }
 
     next();
