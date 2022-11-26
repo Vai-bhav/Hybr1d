32e27bd7eba13382184034a225c57887eb591a74
Vai-bhav <vaibhavkumar.jnva@gmail.com>
2022-11-26 14:56:19 +0530

    create order API validator added

--- modules/buyer/index.js
+++ modules/buyer/index.js
@@ -6,3 +6,4 @@ const controllers = require('./controller');
 
 app.get('/buyer/list-of-sellers', validator.sellerList, auth.authenticateUser, controllers.sellerList);
 app.get('/buyer/seller-catalog/:seller_id', controllers.getSellerCatalogByID);
+// app.post('/buyer/create-order/:seller_id', validator.createOrder);
--- modules/buyer/validator.js
+++ modules/buyer/validator.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 
 const apiReferenceModule = "buyer";
 
-exports.sellerList = sellerList;
+exports.sellerList  = sellerList;
+exports.createOrder = createOrder;
 
 function validate(opts, schema) {
     const validate = schema.validate(opts);
@@ -22,6 +23,31 @@ function sellerList(req, res, next) {
     });
 
     const validateFields = validate(req.query, schema);
+    if(validateFields.error) {
+        return res.status(400).send("INVALID REQUEST");
+    }
+
+    next();
+
+}
+
+async function createOrder(req, res, next) {
+
+    req.apiReference = {
+        apiReferenceModule,
+        api: "createOrder"
+    }
+
+    const schema = Joi.object().keys({
+        access_token: Joi.string().required(),
+        buyer_id    : Joi.number().required(),
+        products    : Joi.array().required({
+            id          : Joi.number().required(),
+            quantity    : Joi.number().required()
+        })
+    })
+
+    const validateFields = validate(req.body, schema);
     if(validateFields.error) {
         return res.status(400).send("INVALID REQUEST");
     }
