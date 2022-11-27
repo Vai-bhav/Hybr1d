dd8fb7968ff773fb6de93d9708cafb1b64d202cb
Vai-bhav <vaibhavkumar.jnva@gmail.com>
2022-11-27 14:53:01 +0530

    seller order API validator

--- modules/seller/index.js
+++ modules/seller/index.js
@@ -4,3 +4,4 @@ const auth       = require('../../auth/auth');
 const controller = require('./controller');
 
 app.post('/seller/create-catalog', validator.createCatalog, auth.authenticateUser, controller.createCatalog);
+// app.get('/seller/orders',          validator.getOrders);
--- modules/seller/validator.js
+++ modules/seller/validator.js
@@ -4,6 +4,7 @@ const Joi = require('joi');
 const apiReferenceModule = "seller";
 
 exports.createCatalog = createCatalog;
+exports.getOrders     = getOrders;
 
 async function validate(opts, schema) {
     const validate = schema.validate(opts);
@@ -23,6 +24,27 @@ async function createCatalog(req, res, next) {
     });
 
     const validateFields = validate(req.body, schema);
+    if(validateFields.error) {
+        return res.status(400).send("INVALID REQUEST");
+    }
+
+    next();
+
+}
+
+async function getOrders(req, res, next) {
+
+    req.apiReference = {
+        apiReferenceModule,
+        api: "getOrders"
+    }
+
+    const schema = Joi.object().keys({
+        seller_id: Joi.number().required()
+    })
+
+    const validateFields = validate(req.query, schema);
+
     if(validateFields.error) {
         return res.status(400).send("INVALID REQUEST");
     }
